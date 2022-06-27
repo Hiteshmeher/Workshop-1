771f7c99ea8e57f2c3e2cce29e75c71e64e2976e
Hitesh Kumar Meher <107098715+Hiteshmeher@users.noreply.github.com>
2022-06-27 21:41:51 +0530

    Update Gambling.java

--- src/gamblingGame/Gambling.java
+++ src/gamblingGame/Gambling.java
@@ -3,30 +3,29 @@ package gamblingGame;
 import java.util.Random;
 
 public class Gambling {
-	/**
-	 * initialize stack is 100
-	 * 
-	 */
 	static int EverydayStack = 100;
 	static int BetStack = 1;
 
 	public static void main(String[] args) {
 		int StackIncrease = 150;
 		int StackDecrease = 50;
+		int win =0;
 		System.out.println("Welcome To Gambler Simulator");
 		System.out.println("Every Day Stack: " + EverydayStack);
 		System.out.println("Bet Price: " + BetStack);
 
-		while (EverydayStack > StackIncrease && EverydayStack < StackDecrease) {
-			double random = Math.random();
-			if (random < 0.5) {
-				System.out.println("win the bet and stack is " + (EverydayStack+=BetStack));
-			
-			} else {
-				System.out.println("Loss the bet and stack is " + (EverydayStack-=BetStack));
-			}
 
-		}
 
-	}
+        while(EverydayStack > StackDecrease && EverydayStack < StackIncrease) {
+        	double rand = Math.random();
+
+            if(rand < 0.5) {
+              //  System.out.println("Bet Win");
+                System.out.println("Win and amount is "+(EverydayStack +=BetStack));
+            }else {
+             //  System.out.println("Bet Loss");
+                System.out.println("Loss and amount is "+(EverydayStack -= BetStack));
+            }
+        }
+    }
 }
