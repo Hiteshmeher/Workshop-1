6a800f7020f5b5b09cccaa8dfa05c4d6aa92ff92
Hiteshmeher <meher.hitesh4@gmail.com>
2022-06-27 21:56:41 +0530

    20 days loss or profit calculate

--- src/gamblingGame/Gambling.java
+++ src/gamblingGame/Gambling.java
@@ -1,10 +1,10 @@
 package gamblingGame;
 
-import java.util.Random;
-
 public class Gambling {
 	/**
-	 * initialize stack is 100
+	 * initialize stack is 100 initialize bet is 1 claculating for 20 days if win
+	 * the stack =stack+50 if loss stack= stack-50 final prining win after 20 days
+	 * wheather it is loss or profit
 	 * 
 	 */
 	static int EverydayStack = 100;
@@ -13,20 +13,30 @@ public class Gambling {
 	public static void main(String[] args) {
 		int StackIncrease = 150;
 		int StackDecrease = 50;
-		System.out.println("Welcome To Gambler Simulator");
-		System.out.println("Every Day Stack: " + EverydayStack);
-		System.out.println("Bet Price: " + BetStack);
+		int win = 0;
+		System.out.println("Welcome To Gambling");
+		System.out.println("Every Day Stack is " + EverydayStack);
+		System.out.println("Betting price is " + BetStack);
+		for (int day = 1; day <= 20; day++) {
 
-		while (EverydayStack > StackIncrease && EverydayStack < StackDecrease) {
-			double random = Math.random();
-			if (random < 0.5) {
-				System.out.println("win the bet and stack is " + (EverydayStack+=BetStack));
-			
-			} else {
-				System.out.println("Loss the bet and stack is " + (EverydayStack-=BetStack));
+			while (EverydayStack > StackDecrease && EverydayStack < StackIncrease) {
+				double rand = Math.random();
+
+				if (rand < 0.5) {
+					System.out.println("win the bet and stack is " + (EverydayStack += BetStack));
+					win = win + 50;
+					break;
+				} else {
+					System.out.println("Loss the bet and stack is " + (EverydayStack -= BetStack));
+					win = win - 50;
+					break;
+				}
 			}
 
+			System.out.println("winning amount is " + win);
+
 		}
+		System.out.println(" final winning amount is " + win);
 
 	}
 }
